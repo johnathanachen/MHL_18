1b0e13898128639e341316d10987b76380787642
Johnathan Chen <jc.swfit@icloud.com>
2017-09-23 13:26:38 -0500

    readme file update

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import re
 import requests
 import bs4 as bs
 import urllib
-"""1. Find a way to get url of first search result
-   2. Build out the Alibaba API side
-   3. Create a function that finds the price differential between the two
-   4. Build out the front-end"""
 
-"""Add keys to the top"""
-
+access_key = 'AKIAJ5KTDL536GDNI57Q'
+secret_key = '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'
+assoc_tag = 'ibafeva-20'
 
 amazon = AmazonAPI(access_key,secret_key, assoc_tag)
 
@@ -41,5 +38,5 @@ def find_price(asin_id):
 
 
 
-# asin_id = get_asin(test_url)
-# price = find_price(asin_id)
+asin_id = get_asin(test_url)
+price = find_price(asin_id)
